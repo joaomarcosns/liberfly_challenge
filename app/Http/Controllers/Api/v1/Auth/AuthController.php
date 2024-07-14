5ee3100501917065b6c608b4d3b13b2e12afa10f
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2024-07-14 13:24:45 -0300

    feat: add login

--- app/Http/Controllers/Api/v1/Auth/AuthController.php
+++ app/Http/Controllers/Api/v1/Auth/AuthController.php
@@ -3,15 +3,38 @@
 namespace App\Http\Controllers\Api\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function login()
+
+
+    public function login(LoginRequest $request)
     {
+        $credentials = $request->only(['email', 'password']);
+
+        $user = User::firstWhere('email', $credentials['email']);
+        if (!$user || !Hash::check($credentials['password'], $user->password)) {
+            return response()->json([
+                'message' => "Invalid username or password"
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Delete old tokens
+        $user->tokens()->delete();
+
+        $expiresAt = now()->addWeek();
+        $token = $user->createToken(name: 'user-token', expiresAt: $expiresAt)->plainTextToken;
+
+        return response()->json([
+            'data' =>  $user,
+            'token' =>  $token
+        ]);
     }
 
     /**
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,101 @@
+<?php
+
+use App\Models\User;
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Symfony\Component\HttpFoundation\Response;
+
+uses(RefreshDatabase::class);
+
+$URL = 'api/v1/auth/login';
+$faker = Faker::create();
+
+it('email max 255', function () use ($URL) {
+    $email = str_repeat('a', 256) . '@example.com';
+    $response = $this->postJson($URL, [
+        'email' => $email,
+        'password' => 'password',
+    ]);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors('email');
+});
+
+it('email min 10', function () use ($URL) {
+    $email = 'a@b.c';
+    $response = $this->postJson($URL, [
+        'email' => $email,
+        'password' => 'password',
+    ]);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors('email');
+});
+
+it('requires a valid email', function () use ($URL, $faker) {
+    $response = $this->postJson($URL, [
+        'email' => 'not-an-email',
+        'password' => 'password',
+    ]);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors('email');
+});
+
+it('password required', function () use ($URL, $faker) {
+    $userData = [
+        'email' => $faker->email(),
+        'password' => '',
+    ];
+
+    $response = $this->postJson($URL, $userData);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJson(
+            fn (AssertableJson $json) => $json->has('errors.password')
+                ->where('errors.password.0', 'The password field is required.')
+                ->etc()
+        );
+
+    $this->assertDatabaseMissing('users', [
+        'password' => $userData['password'],
+    ]);
+});
+
+it('should authenticate user and return token', function () use ($URL, $faker) {
+    $password = $faker->password(11);
+    
+    $user = User::factory()->create([
+        'email' => $faker->safeEmail(),
+        'password' => $password
+    ]);
+
+    $response = $this->postJson($URL, [
+        'email' => $user->email,
+        'password' => $password,
+    ]);
+
+
+    $response->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'email',
+                'email_verified_at',
+                'created_at',
+                'updated_at',
+            ],
+            'token',
+        ]);
+});
+
+it('should return error for invalid credentials', function () use ($URL, $faker) {
+    $response = $this->postJson($URL, [
+        'email' => $faker->safeEmail(),
+        'password' => 'invalidpassword',
+    ]);
+
+    $response->assertStatus(Response::HTTP_BAD_REQUEST)
+        ->assertJson(['message' => 'Invalid username or password']);
+});
